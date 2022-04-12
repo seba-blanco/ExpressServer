b0954ef0c764aea1b5c93e13eb29c8a10ba75135
seba-blanco <sope.blanco@gmail.com>
2022-04-11 21:58:34 -0300

    version con Contenedor separado

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { get } = require('express/lib/response');
 const {Router} = express;
 
+const Contenedor = require("./contenedorAsync");
 const port= 8080;
 
 const app = express();
@@ -14,106 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/api/productos", router);
 
-class contenedor {
-    constructor(fileName) {
-        this.fileName = fileName;
-        this.fileContent = [];
 
-    }
-        
-    readFile = async () => {
-     const content =  await fs.promises.readFile(this.fileName,'utf-8')
-        .then (contenido => { 
-            
-           return JSON.parse(contenido);
-           
-        })
-        
-        .catch(error => {
-        })
-        
-        return content;
-    }
-
-
-    writeFile =async (data) => {
-        
-        await fs.promises.writeFile(this.fileName, JSON.stringify(data, null, 4))
-        .then(res => {console.log("actualizado")})
-        .catch(err => {console.log('no se puedo actualizar', err)})
-
-
-    }
-    
-
-    getAll = async () => {
-        console.log("entre al get all")
-        let datos = await this.readFile().then (prods=> { return prods});
-        
-        return datos;
-    }
-    
-    getById =async (id) => {
-        console.log("entre al get by id")
-        let datos = await this.readFile().then (prods=> {return prods});
-
-        console.log("esto es datos");
-        console.log(datos);
-        return datos.filter(x=> x.id == id);
-    }
-    
-    save =async (object) => {
-        console.log("datos get by id")
-        let datos = await this.readFile().then (prods=> {return prods});
-        console.log(datos)
-        let maxId = Math.max(...datos.map(prod => prod.id), 0);
-        object["id"] = maxId + 1;
-        datos.push(object);
-       
-        this.writeFile(datos);
-
-        return object;
-
-
-    }
-
-    update = async (id, product) => {
-        let datos = await this.readFile().then (prods=> {return prods});
-        
-        let newData = datos.filter(x=> x.id != id);
-
-        product["id"] = id;
-
-        newData.push(product);
-       
-        this.writeFile(newData);
-
-        return product;
-
-
-    }
-
-    deleteById =async(id) => {
-        
-        let datos = await this.readFile().then (prods=> {return prods});
-        
-        let newData = datos.filter(x=> x.id != id);
-        
-      
-        this.writeFile(newData);
-
-    }
-
-    
-    deleteAll = async () => {
-        console.log("entre al delete all")
-        
-        this.writeFile([]);
-    }
-}
 
  
-const archivo = new contenedor('./products.json');
+const archivo = new Contenedor('./products.json');
 
 const server =app.listen(port, () => {
 
