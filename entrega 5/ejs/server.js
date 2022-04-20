1fe957daa62991f3c8fbca88194f0bf6ea9fa773
seba-blanco <sope.blanco@gmail.com>
2022-04-20 16:16:46 -0300

    pug y handlebars

---
+++ entrega 5/ejs/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const Contenedor = require("./contenedorAsync");
+
+// const router = Router();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// app.use("/api/productos", router);
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+app.use(express.static("public"));
+
+
+
+ 
+const archivo = new Contenedor('./products.json');
+
+const server =app.listen(PORT, () => {
+
+    console.log(`servidor http escuchando en el puerto ${server.address().port} `)
+})
+
+server.on("error", error => console.log(`Error en servidor ${error}`));
+
+
+app.get('/productos',async  (req,res) =>{
+    
+    let prods =await archivo.getAll();
+    
+    console.log(prods);
+    res.render('productos.pug', { productos: prods, listExists: true });  
+
+})
+
+app.get('/',async  (req,res) =>{
+    
+    res.render('main.pug', {});  
+
+})
+
+
+//add product to products.json
+app.post("/", async (req, res) => {
+    let newProduct = await archivo.save(req.body);
+    
+     res.redirect('/productos');
+})
+
+
+
+
---
+++ entrega 5/handlebars/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const handlebars = require('express-handlebars');
+
+const Contenedor = require("./contenedorAsync");
+
+// const router = Router();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// app.use("/api/productos", router);
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static("public"));
+
+app.engine(
+    "hbs", 
+    handlebars.engine({
+        extname: ".hbs",
+        defaultLayout: 'index.hbs',
+        layoutsDir: __dirname + "/views/layouts",
+        partialsDir: __dirname + "/views/partials/"
+    })
+);
+
+ 
+const archivo = new Contenedor('./products.json');
+
+const server =app.listen(PORT, () => {
+
+    console.log(`servidor http escuchando en el puerto ${server.address().port} `)
+})
+
+server.on("error", error => console.log(`Error en servidor ${error}`));
+
+
+app.get('/productos',async  (req,res) =>{
+    
+    let prods =await archivo.getAll();
+    
+    console.log(prods);
+    res.render('productos', { productos: prods, listExists: true });  
+
+})
+
+app.get('/',async  (req,res) =>{
+    
+    res.render('main', {});  
+
+})
+
+
+//add product to products.json
+app.post("/", async (req, res) => {
+    console.log("entre al post");
+    console.log(req.body);
+    let newProduct = await archivo.save(req.body);
+     console.log(newProduct);
+     res.redirect('/productos');
+     // res.json({newProduct: newProduct});
+})
+
+
+
+
