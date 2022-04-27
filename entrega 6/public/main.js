f530267706d4d3dd043a9a5e1f2ea175763e95ce
seba-blanco <sope.blanco@gmail.com>
2022-04-27 18:56:26 -0300

    entrega 6

--- entrega 6/public/main.js
+++ entrega 6/public/main.js
@@ -4,16 +4,41 @@ function preventDef(){
     return false;
 }
 
+socket.on('welcome', data => {
+    renderProductos(data.products);
+    renderChat(data.chat);
+
+});
+
+
+
 function renderProductos(data) {
+    
+    const html = data.map((elem, index) => {
+        return(`<tr>
+            <td>${elem.title}</td>
+            <td>${elem.price}</td>
+            <td><img src='${elem.thumbnails}'></img></td>
+        </tr>`)
+    }).join(" ");
+    document.getElementById('tableProd').getElementsByTagName('tbody')[0].innerHTML = html;
+    
+    
+}
+
+
+function renderChat(data) {
+    if (data.length> 0) { 
     const html = data.map((elem, index) => {
         return(`<div>
-            <strong>${elem.title}</strong>:
-            <em>${elem.price}</em>
+            <strong style='color:blue;'>${elem.userName}</strong>:
+            <span style='color:brown;'>[${elem.datetime}]</span>
+            <i style='color:green;'>${elem.message}</i>
         </div>`)
     }).join(" ");
 
-    document.getElementById('lstProducts').innerHTML = html;
-    
+   document.getElementById('lstMsgs').innerHTML = html;
+    }
 }
 
 function addProduct(e) {
@@ -41,26 +66,12 @@ function addChatMessage(e) {
     return false;
 }
 
+
 socket.on('products', data => {
    
     renderProductos(data);
 });
 
-
-function renderChat(data) {
-    const html = data.map((elem, index) => {
-        return(`<div>
-            <strong>${elem.title}</strong>:
-            <em>${elem.price}</em>
-        </div>`)
-    }).join(" ");
-
-    document.getElementById('lstMsgs').innerHTML = html;
-    
-}
-
-
 socket.on('chatMessages', data => {
-    alert('en el mensajes');
-    renderChat(data);
-})
+    renderChat(data.chat);
+});
--- entrega 6/server.js
+++ entrega 6/server.js
@@ -14,6 +14,9 @@ let moment = require('moment');
 
 app.use(express.static('./public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
  
@@ -45,26 +48,28 @@ app.get('/',async  (req,res) =>{
 io.on('connection', async (socket) => {
     console.log('Cliente conectado');
     let allProds = await archivo.getAll();
-    socket.emit('products', allProds);
+    let allMessages = await chat.getMessages();
+    socket.emit('welcome', {products:allProds, chat: allMessages});
    
 
     socket.on('newProduct', async (data) => {
         
         await archivo.save(data);
         let allProds = await archivo.getAll();
-        io.sockets.emit('products', {productos:allProds});
+        io.sockets.emit('products', allProds);
     });
 
     socket.on('newMessage', async (data) => {
-        console.log("andetro del new message");
+       
         data['datetime'] = moment(new Date()).format("DD/MM/YYYY HH:mm:ss");
         await chat.addMessage(data);
         
-        console.log("pase el save message");
+       
         let allMessages = await chat.getMessages();
-        console.log("pase el get messages");
+        console.log("al messages");
+        console.log(allMessages);
         
-        io.sockets.emit('chatMessages', {chats:allMessages});
+        io.sockets.emit('chatMessages', {chat:allMessages});
     })
 })
 
