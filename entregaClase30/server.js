580c1c370007cced84aeb173f13343233aebbc95
seba-blanco <sope.blanco@gmail.com>
2022-07-13 21:16:35 -0300

    con gnix funcionando

--- entregaClase30/server.js
+++ entregaClase30/server.js
@@ -54,7 +54,7 @@ app.use(session({
 
 
 app.use("/api/", randomRouter);
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(passport.initialize())
